f0960d7b6f358b6cfff6052d4c4d36cb5ebf874f
WakelessSloth56 <wsloth56@foxmail.com>
2022-09-10 00:00:57 +0800

    feat(api): IEnchantableEntity

---
+++ src/main/java/org/auioc/mcmod/harmonicenc/api/mixin/common/IMixinProjectile.java
@@ -0,0 +1,7 @@
+package org.auioc.mcmod.harmonicenc.api.mixin.common;
+
+import org.auioc.mcmod.harmonicenc.api.entity.IEnchantableEntity;
+
+public interface IMixinProjectile extends IEnchantableEntity {
+
+}
