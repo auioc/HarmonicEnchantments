d61fc513817a8adc3bbe42032bee9def3c052fff
WakelessSloth56 <wsloth56@foxmail.com>
2022-09-16 20:59:29 +0800

    feat(ench): mob affinity 2

--- src/main/java/org/auioc/mcmod/harmonicench/common/enchantment/impl/MobAffinityEnchantment.java
+++ src/main/java/org/auioc/mcmod/harmonicench/common/enchantment/impl/MobAffinityEnchantment.java
@@ -3,13 +3,18 @@ package org.auioc.mcmod.harmonicench.common.enchantment.impl;
 import org.auioc.mcmod.harmonicench.api.enchantment.AbstractHEEnchantment;
 import org.auioc.mcmod.harmonicench.api.enchantment.ILivingEnchantment;
 import org.auioc.mcmod.harmonicench.server.event.impl.PiglinStanceEvent;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.item.enchantment.EnchantmentCategory;
 import net.minecraft.world.item.enchantment.Enchantments;
+import net.minecraftforge.fml.util.ObfuscationReflectionHelper;
 
-public class MobAffinityEnchantment extends AbstractHEEnchantment implements ILivingEnchantment.PiglinStance {
+public class MobAffinityEnchantment extends AbstractHEEnchantment implements ILivingEnchantment.PiglinStance, ILivingEnchantment.Potion {
 
     public MobAffinityEnchantment() {
         super(Enchantment.Rarity.VERY_RARE, EnchantmentCategory.WEARABLE, new EquipmentSlot[] {EquipmentSlot.HEAD, EquipmentSlot.CHEST, EquipmentSlot.LEGS, EquipmentSlot.FEET}, 1);
@@ -36,6 +41,29 @@ public class MobAffinityEnchantment extends AbstractHEEnchantment implements ILi
             return PiglinStanceEvent.Stance.NEUTRAL;
         }
         return stance;
+    }
+
+    @Override
+    public void onPotionAdded(int lvl, EquipmentSlot slot, Entity source, MobEffectInstance newEffect, MobEffectInstance oldEffect) {
+        if (this.isValidSlot(slot)) {
+            if (newEffect.getEffect() == MobEffects.BAD_OMEN) {
+                setEffectDuration(newEffect, 0);
+            } else if (source != null) {
+                if (source.getType() == EntityType.AXOLOTL && newEffect.getEffect() == MobEffects.REGENERATION) {
+                    setEffectAmplifier(newEffect, 1);
+                } else if (source.getType() == EntityType.DOLPHIN && newEffect.getEffect() == MobEffects.DOLPHINS_GRACE) {
+                    setEffectDuration(newEffect, 10 * 20);
+                }
+            }
+        }
+    }
+
+    private static void setEffectDuration(MobEffectInstance effect, int duration) {
+        ObfuscationReflectionHelper.setPrivateValue(MobEffectInstance.class, effect, duration, "f_19503_");
+    }
+
+    private static void setEffectAmplifier(MobEffectInstance effect, int amplifier) {
+        ObfuscationReflectionHelper.setPrivateValue(MobEffectInstance.class, effect, amplifier, "f_19504_");
     }
 
 }
