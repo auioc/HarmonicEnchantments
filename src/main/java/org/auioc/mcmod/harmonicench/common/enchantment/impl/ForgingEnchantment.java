47f991cdcf3732f2bcc1f0195d6425d0bfcc23a5
WakelessSloth56 <wsloth56@foxmail.com>
2022-09-23 22:52:17 +0800

    feat(ench): forging

--- src/main/java/org/auioc/mcmod/harmonicench/common/enchantment/impl/ForgingEnchantment.java
+++ src/main/java/org/auioc/mcmod/harmonicench/common/enchantment/impl/ForgingEnchantment.java
@@ -1,15 +1,24 @@
 package org.auioc.mcmod.harmonicench.common.enchantment.impl;
 
+import java.util.Map;
+import java.util.UUID;
+import javax.annotation.Nullable;
+import org.auioc.mcmod.arnicalib.utils.java.MathUtil;
 import org.auioc.mcmod.harmonicench.api.enchantment.AbstractHEEnchantment;
+import org.auioc.mcmod.harmonicench.api.enchantment.IAttributeModifierEnchantment;
 import org.auioc.mcmod.harmonicench.common.enchantment.HEEnchantments;
 import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.ai.attributes.Attribute;
+import net.minecraft.world.entity.ai.attributes.AttributeModifier;
+import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.item.ArmorItem;
 import net.minecraft.world.item.ArmorMaterials;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.item.enchantment.EnchantmentCategory;
 import net.minecraft.world.item.enchantment.Enchantments;
 
-public class ForgingEnchantment extends AbstractHEEnchantment {
+public class ForgingEnchantment extends AbstractHEEnchantment implements IAttributeModifierEnchantment {
 
     private static final EnchantmentCategory METALLIC_ARMOR = EnchantmentCategory.create(
         "METALLIC_ARMOR",
@@ -31,6 +40,9 @@ public class ForgingEnchantment extends AbstractHEEnchantment {
         }
     );
 
+    private static final UUID ARMOR_TOUGHNESS_MODIFIER_UUID = UUID.fromString("26E2A6CB-1EA6-401A-BD70-6ECB85D39969");
+    private static final UUID ARMOR_MODIFIER_UUID = UUID.fromString("6EB3BD31-C501-4EFE-BE1E-900A9200A310");
+
     public ForgingEnchantment() {
         super(
             Enchantment.Rarity.RARE,
@@ -50,6 +62,24 @@ public class ForgingEnchantment extends AbstractHEEnchantment {
     @Override
     public int getMaxCost(int lvl) {
         return 41;
+    }
+
+    @Nullable
+    @Override
+    public Map<Attribute, AttributeModifier> getAttributeModifier(int lvl, EquipmentSlot slot, ItemStack itemStack) {
+        if (itemStack.canEquip(slot, null)) {
+            double x = (Math.log((double) itemStack.getBaseRepairCost() + 1) / Math.log(2.0D));
+            if (x > 0.0D) {
+                double bonus = x * MathUtil.sigma(lvl, 1, (double i) -> 1 / (3 * i));
+                return Map.of(
+                    Attributes.ARMOR,
+                    new AttributeModifier(ARMOR_MODIFIER_UUID, this.descriptionId, bonus, AttributeModifier.Operation.ADDITION),
+                    Attributes.ARMOR_TOUGHNESS,
+                    new AttributeModifier(ARMOR_TOUGHNESS_MODIFIER_UUID, this.descriptionId, bonus, AttributeModifier.Operation.ADDITION)
+                );
+            }
+        }
+        return null;
     }
 
 }
