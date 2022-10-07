e5b0022e0c8702da4f83ca1ca46f97c3e2cba800
WakelessSloth56 <wsloth56@foxmail.com>
2022-10-08 00:24:00 +0800

    feat(mobeffect): adjust fall fly speed with weightlessness mob effect

--- src/main/java/org/auioc/mcmod/harmonicench/common/mobeffect/impl/WeightlessnessMobEffect.java
+++ src/main/java/org/auioc/mcmod/harmonicench/common/mobeffect/impl/WeightlessnessMobEffect.java
@@ -16,7 +16,8 @@ public class WeightlessnessMobEffect extends MobEffect {
     public static Vec3 adjustFallFlySpeed(LivingEntity living, Vec3 vec3) {
         int lvl = Math.min(EffectUtils.getEffectLevel(living, HEMobEffects.WEIGHTLESSNESS.get()), 100);
         if (lvl > 0) {
-            return vec3;
+            double vY = vec3.y + Math.abs(vec3.y) * lvl * 0.01D;
+            return new Vec3(vec3.x, vY, vec3.z);
         }
         return vec3;
     }
