3e0583f7794f89298e658cb6882c23ddea307239
WakelessSloth56 <wsloth56@foxmail.com>
2023-01-27 19:54:49 +0800

    feat(api): IMovementInputMobEffect

--- src/main/java/org/auioc/mcmod/harmonicench/client/event/HEClientEventHandler.java
+++ src/main/java/org/auioc/mcmod/harmonicench/client/event/HEClientEventHandler.java
@@ -1,8 +1,11 @@
 package org.auioc.mcmod.harmonicench.client.event;
 
+import org.auioc.mcmod.harmonicench.api.effect.IMovementInputMobEffect;
 import org.auioc.mcmod.harmonicench.utils.EnchantmentPerformer;
+import net.minecraft.client.player.LocalPlayer;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.client.event.MovementInputUpdateEvent;
 import net.minecraftforge.event.entity.player.ItemTooltipEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
@@ -12,6 +15,17 @@ public final class HEClientEventHandler {
     @SubscribeEvent
     public static void onItemTooltip(final ItemTooltipEvent event) {
         EnchantmentPerformer.onItemTooltip(event.getItemStack(), event.getPlayer(), event.getToolTip(), event.getFlags());
+    }
+
+    @SubscribeEvent
+    public static void onMovementInputUpdate(MovementInputUpdateEvent event) {
+        var input = event.getInput();
+        var player = (LocalPlayer) event.getPlayer();
+        for (var entry : player.getActiveEffectsMap().entrySet()) {
+            if (entry.getKey() instanceof IMovementInputMobEffect effect) {
+                effect.onMovementInputUpdate(entry.getValue().getAmplifier(), input, player);
+            }
+        }
     }
 
 }
